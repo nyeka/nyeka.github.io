6cfb957d35d16c16fd0828d3f69ccb631a9f6486
nyoman <nyeka2101@gmail.com>
2022-04-22 15:51:20 +0700

    edit domain name

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -7,44 +7,38 @@ import { FiUsers } from "react-icons/fi";
 export default function About() {
   return (
     <section id="about ">
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
+      <h5> Get To Know </h5> <h2> About Me </h2>{" "}
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
             <img src={Baal} alt="About image" />
-          </div>
-        </div>
+          </div>{" "}
+        </div>{" "}
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
               <FaAward className="about__icon" />
-              <h5>Expericence</h5>
-              <small>I'm still learning</small>
+              <h5> Expericence </h5> <small> I 'm still learning</small>{" "}
             </article>
-
             <article className="about__card">
               <FiUsers className="about__icon" />
-              <h5>Expericence</h5>
-              <small>I'm still learning</small>
+              <h5> Expericence </h5> <small> I 'm still learning</small>{" "}
             </article>
-
             <article className="about__card">
               <FaAward className="about__icon" />
-              <h5>Expericence</h5>
-              <small>I'm still learning</small>
-            </article>
-          </div>
-          <p>
+              <h5> Expericence </h5> <small> I 'm still learning</small>{" "}
+            </article>{" "}
+          </div>{" "}
+          <p className="test">
             Hi, My Name is Nyoman Eka Swardita, Iam currently learning dart
-            language, especialy on flutter development
-          </p>
+            language, especialy on flutter development{" "}
+          </p>{" "}
           <a href="https://wa.me/+6282289994696" className="btn btn-primary">
             {" "}
-            Let's Talk
-          </a>
-        </div>
-      </div>
+            Let 's Talk{" "}
+          </a>{" "}
+        </div>{" "}
+      </div>{" "}
     </section>
   );
 }
