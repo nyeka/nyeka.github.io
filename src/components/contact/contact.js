efb8ef03de621c5754dc4c75b8a823789f61749b
nyoman <nyeka2101@gmail.com>
2022-04-25 22:38:35 +0700

    edit message button

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -6,17 +6,39 @@ import emailjs from "emailjs-com";
 
 export default function Contact() {
   const form = useRef();
+  const loading = document.querySelector(".btn-loading");
+  const kirim = document.querySelector(".btn-kirim");
+  const alert = document.querySelector(".my-alert");
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(
-      "service_gpz2f3o",
-      "template_tsoa10k",
-      form.current,
-      "jinwk4G-2E93lm7Mm"
-    );
+    emailjs
+      .sendForm(
+        "service_0a9jvrb",
+        "template_clm139w",
+        form.current,
+        "Oj47iMv6ywYMjFxYd"
+      )
+      .then(
+        (result) => {
+          loading.classList.toggle("d-none");
+          kirim.classList.toggle("d-none");
+          alert.classList.toggle("d-none");
+          form.reset();
+          console.log(result.text);
+          e.target.reset();
+          e.form.reset();
+          e.preventDefault();
+          e.form.preventDefault();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
     e.target.reset();
+    e.form.reset();
+    form.reset();
   };
 
   return (
@@ -30,7 +52,11 @@ export default function Contact() {
             <MdOutlineEmail />
             <h4>Email</h4>
             <h5>nyeka2101@gmail.com</h5>
-            <a href="mailto:nyeka2101@gmail.com" target="_blank" rel="noreferrer">
+            <a
+              href="mailto:nyeka2101@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
               Send a message
             </a>
           </article>
@@ -46,7 +72,8 @@ export default function Contact() {
             <h5>+6282289994696</h5>
             <a
               href="https://api.whatsapp.com/send?phone=+6282289994696"
-              target="_blank" rel="noreferrer"
+              target="_blank"
+              rel="noreferrer"
             >
               Send a message
             </a>
@@ -66,9 +93,33 @@ export default function Contact() {
             placeholder="Your Message"
             required
           ></textarea>
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary btn-kirim">
             Send Message
           </button>
+          <button
+            class="btn btn-primary d-none btn-loading"
+            type="button"
+            disabled
+          >
+            <span
+              class="spinner-border spinner-border-sm"
+              role="status"
+              aria-hidden="true"
+            ></span>
+            Loading...
+          </button>
+          <div
+            class="alert alert-success d-none alert-dismissible fade show  my-alert"
+            role="alert"
+          >
+            <strong>Terimakasih!</strong> Pesan Anda Telah Dikirim.
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+            ></button>
+          </div>
         </form>
       </div>
     </section>
